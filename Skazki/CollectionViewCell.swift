69848d8fb834a2da4b9aade7aa83fc9edee79f17
Trant Solo <solotrant@gmail.com>
2017-11-01 04:02:30 +0500

    first commit

---
+++ Skazki/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  Skazki
+//
+//  Created by Trant Solo on 29.08.17.
+//  Copyright © 2017 tuchka. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Skazki/StoryData.swift
@@ -0,0 +1,9 @@
+//
+//  StoryData.swift
+//  Skazki
+//
+//  Created by Trant Solo on 15.09.17.
+//  Copyright © 2017 tuchka. All rights reserved.
+//
+
+import Foundation
